07ecdb90cc5159ca70be3135de1ccf71e1a1235f
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-07-09 11:24:45 +0800

    change SDK, format game activity view

--- app/src/main/java/nus/iss/androidca/GameActivity.java
+++ app/src/main/java/nus/iss/androidca/GameActivity.java
@@ -3,12 +3,32 @@ package nus.iss.androidca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView txtScore;
+    int matchCounter;
+    int mSeconds;
+    int mMinutes;
+    int mHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        txtScore = findViewById(R.id.matchCount);
+        txtScore.setText(String.valueOf(matchCounter));
+
+        matchCounter = 0;
+
+        txtScore.setText(matchCounter + " of 6 matches");
+
+        runTimer();
+    }
+
+    private void runTimer(){
+
     }
 }
