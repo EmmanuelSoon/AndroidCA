119f01cce6a59ec88b45484786df43085bc6f0cb
lyc <280369571@qq.com>
2022-07-09 23:27:16 +0800

    Game Activity added

--- app/src/main/java/nus/iss/androidca/GameActivity.java
+++ app/src/main/java/nus/iss/androidca/GameActivity.java
@@ -1,12 +1,18 @@
 package nus.iss.androidca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements GameFragment.IGameFragment{
 
     TextView txtScore;
     int matchCounter;
@@ -16,18 +22,25 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
         txtScore = findViewById(R.id.matchCount);
         txtScore.setText(String.valueOf(matchCounter));
-
         startTime = 0;
         matchCounter = 0;
-
         txtScore.setText(matchCounter + " of 6 matches");
-
         runTimer();
+        // send data to our fragment
+        Bitmap[] bitmaps = new Bitmap[6];
+        bitmaps[0] = BitmapFactory.decodeResource(this.getResources(), R.drawable.card1);
+        bitmaps[1] = BitmapFactory.decodeResource(this.getResources(), R.drawable.card2);
+        bitmaps[2] = BitmapFactory.decodeResource(this.getResources(), R.drawable.card3);
+        bitmaps[3] = BitmapFactory.decodeResource(this.getResources(), R.drawable.card4);
+        bitmaps[4] = BitmapFactory.decodeResource(this.getResources(), R.drawable.card5);
+        bitmaps[5] = BitmapFactory.decodeResource(this.getResources(), R.drawable.card6);
+        Bitmap defaultBitmaps = BitmapFactory.decodeResource(this.getResources(), R.drawable.cardback);
+        FragmentManager fm = getSupportFragmentManager();
+        GameFragment fragment = (GameFragment) fm.findFragmentById(R.id.fragment_game);
+        fragment.setBitmaps(bitmaps, defaultBitmaps);
     }
-
 
     private void runTimer() {
         TextView txtTime = findViewById(R.id.timer);
@@ -42,9 +55,21 @@ public class GameActivity extends AppCompatActivity {
                 seconds = seconds % 60;
                 minutes = minutes % 60;
                 txtTime.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
-
                 handler.postDelayed(this, 1000);
             }
         });
     }
+    @Override
+    public void itemClicked(String content) {
+        if (content.equals("match")) {
+            matchCounter++;
+            txtScore.setText(matchCounter + " of 6 matches");
+            Toast.makeText(this, "Match!", Toast.LENGTH_SHORT).show();
+        }
+        else if(content.equals("over")) {
+            txtScore.setText("Game Over!");
+            Toast.makeText(this, "You Win!", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
--- app/src/main/java/nus/iss/androidca/GameFragment.java
+++ app/src/main/java/nus/iss/androidca/GameFragment.java
@@ -1,64 +1,211 @@
 package nus.iss.androidca;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.UiThread;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GameFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class GameFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class GameFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Bitmap[] Bitmaps = null;
+    private Bitmap defaultBitmap = null;
+    private int size = 0;
+    private int matchedPairs = 0;
+    private int firstClicked = -1;
+    private List<Integer> board = new ArrayList<>();
+    private List<ImageButton> btns = new ArrayList<>();
 
-    public GameFragment() {
-        // Required empty public constructor
+    private boolean isGameOver() {
+        return size == matchedPairs;
+    }
+    private void initBoard() {
+        for (int i = 0; i < size; i++) {
+            board.add(i);
+            board.add(i);
+        }
+        Collections.shuffle(board);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GameFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GameFragment newInstance(String param1, String param2) {
-        GameFragment fragment = new GameFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public GameFragment() { }
+    public void setBitmaps(Bitmap[] Bitmaps, Bitmap defaultBitmap) {
+        this.size = Bitmaps.length;
+        this.Bitmaps = Bitmaps;
+        this.defaultBitmap = defaultBitmap;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            initBtn(view);
+            initBoard();
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+      return inflater.inflate(R.layout.fragment_game, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        for (int i = 0; i < btns.size(); i++) {
+            ImageButton btn = btns.get(i);
+            if (btn.getId() == id) {
+                // i is the index of the button that we set start from 0
+                handleClick(i);
+                break;
+            }
+        }
+    }
+    private void handleClick(int btnIndex){
+        if (firstClicked < 0) {
+            // if it is first click
+            firstClicked = btnIndex;
+            ImageButton btn = btns.get(btnIndex);
+            btn.setImageBitmap(Bitmaps[board.get(btnIndex)]);
+            //Temporarily disable the button that we just clicked
+            btn.setEnabled(false);
+        }
+        else {
+            //check whether they are matching
+            if (board.get(firstClicked).equals(board.get(btnIndex))) {
+                ImageButton btn = btns.get(btnIndex);
+                btn.setImageBitmap(Bitmaps[board.get(btnIndex)]);
+                btn.setEnabled(false);
+                matchedPairs++;
+                iGameFragment.itemClicked("match");
+                if (isGameOver()) {
+                    //send congrats Msg
+                    iGameFragment.itemClicked("over");
+                }
+                clearFirstClick();
+            }
+            else {
+                //if 2 images are not matching
+                ImageButton btn = btns.get(btnIndex);
+                btn.setImageBitmap(Bitmaps[board.get(btnIndex)]);
+                btn.setEnabled(false);
+
+                new CountDownTimer(1000, 1000) {
+                    @Override
+                    public void onTick(long l) {
+                        //Do nothing
+                    }
+                    @Override
+                    public void onFinish() {
+                        reverseBackImage(firstClicked);
+                        reverseBackImage(btnIndex);
+                        clearFirstClick();
+                    }
+                }.start();
+            }
+        }
+    }
+
+    private void reverseBackImage(int index) {
+        ImageButton btn = btns.get(index);
+        btn.setImageBitmap(defaultBitmap);
+        btn.setEnabled(true);
+    }
+
+    public interface IGameFragment{
+        void itemClicked(String content);
+    }
+    private IGameFragment iGameFragment;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        iGameFragment = (IGameFragment) context;
+    }
+
+    private void initBtn(View v) {
+        ImageButton imageButton0 = v.findViewById(R.id.card0);
+        if (imageButton0 != null) {
+            imageButton0.setOnClickListener(this);
+            btns.add(imageButton0);
+        }
+        ImageButton imageButton1 = v.findViewById(R.id.card1);
+        if (imageButton1 != null) {
+            imageButton1.setOnClickListener(this);
+            btns.add(imageButton1);
+        }
+        ImageButton imageButton2 = v.findViewById(R.id.card2);
+        if (imageButton2 != null) {
+            imageButton2.setOnClickListener(this);
+            btns.add(imageButton2);
+        }
+        ImageButton imageButton3 = v.findViewById(R.id.card3);
+        if (imageButton3 != null) {
+            imageButton3.setOnClickListener(this);
+            btns.add(imageButton3);
+        }
+        ImageButton imageButton4 = v.findViewById(R.id.card4);
+        if (imageButton4 != null) {
+            imageButton4.setOnClickListener(this);
+            btns.add(imageButton4);
+        }
+        ImageButton imageButton5 = v.findViewById(R.id.card5);
+        if (imageButton5 != null) {
+            imageButton5.setOnClickListener(this);
+            btns.add(imageButton5);
+        }
+        ImageButton imageButton6 = v.findViewById(R.id.card6);
+        if (imageButton6 != null) {
+            imageButton6.setOnClickListener(this);
+            btns.add(imageButton6);
+        }
+        ImageButton imageButton7 = v.findViewById(R.id.card7);
+        if (imageButton7 != null) {
+            imageButton7.setOnClickListener(this);
+            btns.add(imageButton7);
+        }
+        ImageButton imageButton8 = v.findViewById(R.id.card8);
+        if (imageButton8 != null) {
+            imageButton8.setOnClickListener(this);
+            btns.add(imageButton8);
+        }
+        ImageButton imageButton9 = v.findViewById(R.id.card9);
+        if (imageButton9 != null) {
+            imageButton9.setOnClickListener(this);
+            btns.add(imageButton9);
+        }
+        ImageButton imageButton10 = v.findViewById(R.id.card10);
+        if (imageButton10 != null) {
+            imageButton10.setOnClickListener(this);
+            btns.add(imageButton10);
+        }
+        ImageButton imageButton11 = v.findViewById(R.id.card11);
+        if (imageButton11 != null) {
+            imageButton11.setOnClickListener(this);
+            btns.add(imageButton11);
+        }
+    }
+
+    public void clearFirstClick() {
+        this.firstClicked = -1;
     }
 }
